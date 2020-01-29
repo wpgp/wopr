fdcafe725d956c813404d49da397f4928562496a
Doug Leasure <doug.leasure@gmail.com>
2020-01-29 19:04:42 +0000

    update example data

--- wd/code/1 download data.R
+++ wd/code/1 download data.R
@@ -15,7 +15,7 @@ library('wopr') # devtools::load_all('pkg')
 catalogue <- getCatalogue()
 
 # download all data from entire catalogue
-downloadData(catalogue)
+downloadData(catalogue, outdir='out/wopr_downloads')
 
 # download NGA Population v1.0
 catalogue_sub <- subset(catalogue, 
--- wd/code/2 query locations.R
+++ wd/code/2 query locations.R
@@ -19,17 +19,6 @@ library('wopr') # devtools::load_all('pkg')
 # api key
 key <- 'zWrlmcPDRXWFQbsMHsKUOphSmYTegSrw'
 
-##---- example polygons from GeoJSON ----##
-# Lagos
-geojson <- '{"type":"FeatureCollection","features":[{"type":"Feature","properties":{},"geometry":{"type":"Polygon","coordinates":[[[3.308258056640625,6.701434474782401],[3.27392578125,6.704162283788004],[3.22723388671875,6.689159145509243],[3.190155029296875,6.6114082535287215],[3.201141357421875,6.5118147063479],[3.264312744140625,6.485889844658782],[3.3563232421875,6.503628052315478],[3.404388427734375,6.558203219021767],[3.37005615234375,6.646875098291585],[3.308258056640625,6.701434474782401]]]}}]}'
-
-# Kinshasa
-geojson <- '{"type":"FeatureCollection","features":[{"type":"Feature","properties":{},"geometry":{"type":"Polygon","coordinates":[[[15.32661437988281,-4.296771016800865],[15.28575897216797,-4.294374508461301],[15.24250030517578,-4.3245014930191905],[15.22430419921875,-4.426171173838907],[15.334167480468748,-4.447051107498101],[15.376052856445312,-4.367293443949956],[15.32661437988281,-4.296771016800865]]]}}]}'
-geojson <- '{"type":"FeatureCollection","features":[{"type":"Feature","properties":{},"geometry":{"type":"Polygon","coordinates":[[[16.94091796875,-4.127285323245357],[17.5341796875,-4.127285323245357],[17.5341796875,-3.5572827265412794],[16.94091796875,-3.5572827265412794],[16.94091796875,-4.127285323245357]]]}}]}'
-
-# convert to sf
-feature <- geojsonsf::geojson_sf(geojson)
-
 
 ##---- population estimates for a single polygon ----##
 
@@ -38,7 +27,7 @@ N <- getPop(feature=wopr_polys[1,],
             country='NGA', 
             ver='1.2',
             key=key,
-            production=F)
+            production=T)
 
 # summarize population total
 summaryPop(N, confidence=0.95, tails=2, popthresh=5e6)
@@ -50,7 +39,7 @@ N <- getPop(feature=wopr_polys[1,],
             country='NGA', 
             ver='1.2',
             agesex=c('f0','f1','m0','m1'),
-            production=F,
+            production=T,
             key=key)
 
 # summarize population total
@@ -62,8 +51,8 @@ summaryPop(N, confidence=0.95, tails=2, popthresh=5e6)
 N <- getPop(feature=wopr_points[1,], 
             country='NGA', 
             ver='1.2',
-            production=F,
-            agesex=c('m1','m5','f1','f5'),
+            production=T,
+            #agesex=c('m1','m5','f1','f5'),
             key=key)
 
 # summarize population total
@@ -72,22 +61,19 @@ summaryPop(N, confidence=0.95, tails=2, popthresh=100)
 
 ##---- population totals for multiple polygons ----##
 
-# polygons from shapefile
-features <- st_read(dsn='in', layer='features')
-
 # get population totals
-totals <- woprize(features, 
-                         country='NGA', 
-                         ver=1.2,
-                         # agesex=c('m0','m1','f0','f1'),
-                         confidence=0.95,
-                         tails=2,
-                         popthresh=5e3,
-                         spatialjoin=T,
-                         timeout=2*60*60,
-                         key=key,
-                         production=F
-                         )
+totals <- woprize(wopr_polys, 
+                  country='NGA', 
+                  ver='1.1',
+                  # agesex=c('m0','m1','f0','f1'),
+                  confidence=0.95,
+                  tails=2,
+                  popthresh=5e3,
+                  spatialjoin=T,
+                  timeout=2*60*60,
+                  key=key,
+                  production=F
+                  )
 totals
 
 # map results
@@ -103,22 +89,31 @@ write.csv(st_drop_geometry(totals), file='out/poptotals.csv', row.names=F)
 
 ##---- population sizes for multiple point locations ----##
 
-# points from shapefile
-features <- suppressWarnings(st_centroid(st_read(dsn='in', layer='features')))
-
 # get population totals
-totals <- woprize(features, 
-                         country='NGA', 
-                         ver=1.2,
-                         # agesex=c('m0','m1','f0','f1'),
-                         confidence=0.95,
-                         tails=2,
-                         agesex=c('m1','m5','f1','f5'),
-                         popthresh=5e3,
-                         spatialjoin=T,
-                         timeout=2*60*60,
-                         key=key,
-                         production=F
-)
+totals <- woprize(wopr_points, 
+                  country='NGA', 
+                  ver=1.2,
+                  # agesex=c('m0','m1','f0','f1'),
+                  confidence=0.95,
+                  tails=2,
+                  agesex=c('m1','m5','f1','f5'),
+                  popthresh=5e3,
+                  spatialjoin=T,
+                  timeout=2*60*60,
+                  key=key,
+                  production=T
+                  )
 
 head(totals[,c('mean','median','lower','upper')])
+
+
+##---- example polygons from GeoJSON ----##
+# Lagos
+geojson <- '{"type":"FeatureCollection","features":[{"type":"Feature","properties":{},"geometry":{"type":"Polygon","coordinates":[[[3.308258056640625,6.701434474782401],[3.27392578125,6.704162283788004],[3.22723388671875,6.689159145509243],[3.190155029296875,6.6114082535287215],[3.201141357421875,6.5118147063479],[3.264312744140625,6.485889844658782],[3.3563232421875,6.503628052315478],[3.404388427734375,6.558203219021767],[3.37005615234375,6.646875098291585],[3.308258056640625,6.701434474782401]]]}}]}'
+
+# Kinshasa
+geojson <- '{"type":"FeatureCollection","features":[{"type":"Feature","properties":{},"geometry":{"type":"Polygon","coordinates":[[[15.32661437988281,-4.296771016800865],[15.28575897216797,-4.294374508461301],[15.24250030517578,-4.3245014930191905],[15.22430419921875,-4.426171173838907],[15.334167480468748,-4.447051107498101],[15.376052856445312,-4.367293443949956],[15.32661437988281,-4.296771016800865]]]}}]}'
+geojson <- '{"type":"FeatureCollection","features":[{"type":"Feature","properties":{},"geometry":{"type":"Polygon","coordinates":[[[16.94091796875,-4.127285323245357],[17.5341796875,-4.127285323245357],[17.5341796875,-3.5572827265412794],[16.94091796875,-3.5572827265412794],[16.94091796875,-4.127285323245357]]]}}]}'
+
+# convert to sf
+feature <- geojsonsf::geojson_sf(geojson)
