5f7c1c2edb2fbe3b34d02e693d283df7370c1924
Doug Leasure <doug.leasure@gmail.com>
2020-02-01 23:19:59 +0000

    Claire's review 2

--- pkg/R/summaryPop.R
+++ pkg/R/summaryPop.R
@@ -26,7 +26,7 @@ summaryPop <- function(N, confidence=0.95, tails=2, popthresh=NA){
   round0_cols <- c('mean','median','lower','upper')
   round3_cols <- c('abovethresh')
   
-  result[,round0_cols] <- round(result[,round0_cols])
+  result[,round0_cols] <- as.integer(round(result[,round0_cols]))
   result[,round3_cols] <- round(result[,round3_cols], 3)
   
   return(result)
