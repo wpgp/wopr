80929dd36c99dd8792984af3e7bf31eeaaf4a745
Doug Leasure <doug.leasure@gmail.com>
2020-02-23 01:04:48 +0000

    Update map.R

--- R/map.R
+++ R/map.R
@@ -5,12 +5,24 @@
 
 map <- function(country, version, localTiles=F) {
   
-  leaflet(options = leafletOptions(minZoom=6, maxZoom=17)) %>%
+  leaflet(options = leafletOptions(minZoom=1, maxZoom=17)) %>%
     
     # base maps
     addProviderTiles(provider='Esri.WorldImagery', group='Satellite') %>% 
-    addProviderTiles(provider='Esri.NatGeoWorldMap', group='Map') %>%
-    addProviderTiles(provider='CartoDB.DarkMatter', group='Dark') %>% # 'Esri.WorldGrayCanvas'
+    # addProviderTiles(provider='Esri.NatGeoWorldMap', group='Map') %>%
+    # addProviderTiles(provider='CartoDB.DarkMatter', group='Dark') %>% # 'Esri.WorldGrayCanvas'
+    
+    # DarkMatter tiles
+    addTiles(urlTemplate='https://api.maptiler.com/maps/darkmatter/{z}/{x}/{y}.png?key=gVR5ppSzDLVxvDV30OUT',
+             attribution='<a href="https://www.maptiler.com/copyright/" target="_blank">© MapTiler</a> <a href="https://www.openstreetmap.org/copyright" target="_blank">© OpenStreetMap contributors</a>',
+             group='Dark',
+             tileOptions(minZoom=1, maxZoom=14, tms=FALSE, opacity=1)) %>%
+    
+    # OSM tiles
+    addTiles(urlTemplate='https://api.maptiler.com/maps/basic/{z}/{x}/{y}.png?key=gVR5ppSzDLVxvDV30OUT',
+             attribution='<a href="https://www.maptiler.com/copyright/" target="_blank">© MapTiler</a> <a href="https://www.openstreetmap.org/copyright" target="_blank">© OpenStreetMap contributors</a>',
+             group='Map',
+             tileOptions(minZoom=1, maxZoom=14, tms=FALSE, opacity=1)) %>%
     
     # population tiles
     addTiles(urlTemplate=ifelse(localTiles,
@@ -18,7 +30,7 @@ map <- function(country, version, localTiles=F) {
                                 file.path('https://tiles.worldpop.org/wopr',country,'population',version,'population/{z}/{x}/{y}.png')), 
              group='Population',
              layerId='tiles_population',
-             options=tileOptions(minZoom=6, maxZoom=14, tms=FALSE, opacity=1),
+             options=tileOptions(minZoom=1, maxZoom=14, tms=FALSE, opacity=0.6),
              attribution='<a href="http://www.worldpop.org">WorldPop, University of Southampton</a>'
              ) %>%
     
