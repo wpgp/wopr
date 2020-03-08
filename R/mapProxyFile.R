c0deb704ae9327545dd43a0e1e242004770062a6
Doug Leasure <doug.leasure@gmail.com>
2020-03-08 18:40:34 +0000

    Update mapProxyFile.R

--- R/mapProxyFile.R
+++ R/mapProxyFile.R
@@ -8,17 +8,35 @@ mapProxyFile <- function(features, map_zoom){
   
   # feature bounding box
   bbox <- sf::st_bbox(features)
-
-  # update map
-  leafletProxy('map') %>% 
+  
+  if(class(features$geometry)[1] %in% c('sfc_POLYGON','sfc_MULTIPOLYGON')){
     
-    # add polygons
-    addPolygons(data=features, group='Upload File') %>%
+    # update map
+    leafletProxy('map') %>% 
+      
+      # add polygons
+      addPolygons(data=features, 
+                  group='Upload File') %>%
+      # fit bounds
+      setView(lng=mean(bbox[c('xmin','xmax')]), 
+              lat=mean(bbox[c('ymin','ymax')]), 
+              zoom=max(map_zoom, 10))
+  }
+  
+  if(class(features$geometry)[1] %in% c('sfc_POINT','sfc_MULTIPOINT')){
     
-    # fit bounds
-    setView(lng=mean(bbox[c('xmin','xmax')]), 
-            lat=mean(bbox[c('ymin','ymax')]), 
-            zoom=max(map_zoom, 9))
+    # update map
+    leafletProxy('map') %>% 
+      
+      # add polygons
+      addCircles(data=features,
+                 radius=56,
+                 group='Upload File') %>%
+      # fit bounds
+      setView(lng=mean(bbox[c('xmin','xmax')]), 
+              lat=mean(bbox[c('ymin','ymax')]), 
+              zoom=max(map_zoom, 10))
+  }
 }
 
 
